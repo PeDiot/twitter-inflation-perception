07160de988dae59607674eafaeba25578384db13
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-01-27 12:18:39 +0100

    functions for data labelling

--- lib/annotate.py
+++ lib/annotate.py
@@ -3,7 +3,9 @@ Helpful function for tweet annotation."""
 
 from pandas.core.frame import DataFrame
 from pandas.core.series import Series
-from typing import Tuple 
+from typing import Tuple, List
+
+import numpy as np 
 
 def extract_random_tweets(df: DataFrame, prop: float=.011) -> Tuple:
     """Description. Extract random tweets from Twitter dataset.
@@ -15,15 +17,30 @@ def extract_random_tweets(df: DataFrame, prop: float=.011) -> Tuple:
     Returns: 
         - indices of selected tweets
         - selected tweets"""
+    
+    if "lexical_field" not in df.columns: 
+        raise ValueError("lexical_field not in columns.")
 
+    if "text" not in df.columns:
+        raise ValueError("text not in columns.")
+    
     def _select_samples(x: Series) -> Series:
         n = x.shape[0]
         return x.sample(n=int(n * prop), replace=False)
 
     grouped = df.groupby("lexical_field",as_index=False)
-    sampled_df = grouped.apply().reset_index(level=0, drop=True)
+    sampled_df = grouped.apply(_select_samples).reset_index(level=0, drop=True)
     
     idxs = sampled_df.index.to_list()
     tweets = sampled_df["text"].to_list()
 
     return idxs, tweets
+
+def add_labels(df: DataFrame, idxs: List, labels: List) -> DataFrame:
+    """Description. Add 'Label' column to Twitter datasets.""" 
+
+    labelled_df = df.copy()    
+    mask = labelled_df.index.isin(idxs)
+    labelled_df.loc[mask, "Label"] = labels
+
+    return labelled_df
