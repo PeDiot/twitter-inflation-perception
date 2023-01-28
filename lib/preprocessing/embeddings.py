c7594d363a1cb3c959235bc915e9c2683850a955
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-01-28 16:59:16 +0100

    updates

--- lib/preprocessing/embeddings.py
+++ lib/preprocessing/embeddings.py
@@ -1,6 +1,18 @@
 """Description. Transform text from tweets into embeddings. 
 
 See Hugging Face for more details: https://huggingface.co/dangvantuan/sentence-camembert-large
+
+Example: 
+    
+>>> from lib.preprocessing.embeddings import text_to_embeddings, load_transformer
+>>> import pickle as pkl
+>>> path = "./backup/data/"
+>>> file_path = path+"tweets_preprocessed.pkl"
+>>> with open(file_path, "rb") as f: tweets_preprocessed = pkl.load(f)
+>>> tweets = tweets_preprocessed["cleaned"]
+>>> model = load_transformer()
+>>> embeddings = text_to_embeddings(model, tweets, file_path=path+"embeddings.npy")
+Batches:   1%|▋                                                                            | 24/2902 [01:21<2:54:49,  3.64s/it]
 """
 
 import numpy as np 
@@ -15,19 +27,7 @@ def load_transformer(model_name: str="dangvantuan/sentence-camembert-large") -> 
 
 def text_to_embeddings(model: SentenceTransformer, sentences: List, file_path: Optional[str]=None) -> np.ndarray: 
     """Description. 
-    Convert list of sentences into numerical numpy array and save as npy file.
-    
-    Example: 
-    
-    >>> from lib.preprocessing.embeddings import text_to_embeddings, load_transformer
-    >>> import pickle as pkl
-    >>> path = "./backup/data/"
-    >>> file_path = path+"tweets_preprocessed.pkl"
-    >>> with open(file_path, "rb") as f: tweets_preprocessed = pkl.load(f)
-    >>> tweets = tweets_preprocessed["cleaned"]
-    >>> model = load_transformer()
-    >>> embeddings = text_to_embeddings(model, tweets, file_path=path+"embeddings.npy")
-    Batches:   1%|▋                                                                            | 24/2902 [01:21<2:54:49,  3.64s/it]"""
+    Convert list of sentences into numerical numpy array and save as npy file."""
 
     embeddings = model.encode(sentences, show_progress_bar=True)
 
